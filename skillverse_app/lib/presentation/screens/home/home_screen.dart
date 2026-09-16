fcebd7d0b52c2ac7f7a6b7402471305cf175f94b
crystalhere3638-max <crystalhere3638@gmail.com>
2026-09-16 21:03:09 +0530

    Update home_screen.dart

--- skillverse_app/lib/presentation/screens/home/home_screen.dart
+++ skillverse_app/lib/presentation/screens/home/home_screen.dart
@@ -88,8 +88,8 @@ class _HomeScreenState extends State<HomeScreen> {
         slivers: [
       const SliverToBoxAdapter(child: SizedBox(height: 150, child: ColoredBox(color: Colors.orange, child: Center(child: Text('TEST', style: TextStyle(fontSize: 30, color: Colors.black)))))),
             
-          /*const SliverToBoxAdapter(child: OfflineBanner()),
-          SliverToBoxAdapter(
+          const SliverToBoxAdapter(child: OfflineBanner()),
+          /*SliverToBoxAdapter(
             child: _Header(username: username, profile: profile),
           ),
           SliverToBoxAdapter(
