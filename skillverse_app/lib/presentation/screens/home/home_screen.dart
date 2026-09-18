c56ed4a52450ff662026699e63d7713f295cd6e0
crystalhere3638-max <crystalhere3638@gmail.com>
2026-09-18 12:49:28 +0530

    Update home_screen.dart

--- skillverse_app/lib/presentation/screens/home/home_screen.dart
+++ skillverse_app/lib/presentation/screens/home/home_screen.dart
@@ -218,7 +218,6 @@ class _Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return const SizedBox(height: 50, child: ColoredBox(color: Colors.pink)); 
     return Padding(
       padding: const EdgeInsets.fromLTRB(20, 16, 20, 0),
       child: Column(
